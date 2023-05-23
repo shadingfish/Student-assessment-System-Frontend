0176cd5d1b5f5c0568edb5b8324e9f0756820571
wanglingyu <1512528122@qq.com>
2023-05-23 18:27:45 +0800

    完善分页功能

--- src/views/admins/ImportListView.vue
+++ src/views/admins/ImportListView.vue
@@ -1,48 +1,70 @@
 <template>
   <div>
-    <el-button type="text" @click="dialogVisible = true">导入学生</el-button>
+    <el-container>
+      <el-header
+        ><el-button type="primary" @click="dialogVisible = true"
+          >导入学生</el-button
+        >
 
-    <el-dialog title="导入学生" :visible.sync="dialogVisible" width="40%">
-      <el-upload
-        ref="upload"
-        class="upload-demo"
-        drag
-        :action="uploadUrl"
-        :limit="1"
-        accept=".xlsx, .xls"
-        :auto-upload="false"
-        :on-change="handleFileChange"
+        <el-dialog title="导入学生" :visible.sync="dialogVisible" width="40%">
+          <el-upload
+            ref="upload"
+            class="upload-demo"
+            drag
+            :action="uploadUrl"
+            :limit="1"
+            accept=".xlsx, .xls"
+            :auto-upload="false"
+            :on-change="handleFileChange"
+          >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或<em>点击上传</em>
+            </div>
+            <div class="el-upload__tip text-center" slot="tip">
+              <span>仅允许导入xls、xlsx格式文件。</span>
+            </div>
+          </el-upload>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="uploadFile">上 传</el-button>
+          </div>
+        </el-dialog></el-header
       >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip text-center" slot="tip">
-          <span>仅允许导入xls、xlsx格式文件。</span>
-        </div>
-      </el-upload>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="uploadFile">上 传</el-button>
-      </div>
-    </el-dialog>
-
-    <el-table v-if="pagedStudentList.length > 0" :data="pagedStudentList" border>
-      <el-table-column prop="card_id" label="学号"></el-table-column>
-      <el-table-column prop="name" label="姓名"></el-table-column>
-      <el-table-column prop="school" label="学院"></el-table-column>
-      <el-table-column prop="grade" label="年级"></el-table-column>
-      <el-table-column prop="stu_class" label="班级"></el-table-column>
-      <el-table-column prop="major" label="专业"></el-table-column>
-    </el-table>
-
-    <el-pagination
-      v-if="studentList.length > 0"
-      background
-      :current-page="currentPage"
-      :page-size="pageSize"
-      :total="totalStudents"
-      @current-change="handlePageChange"
-    >
-    </el-pagination>
+      <el-main
+        ><el-table v-if="studentList.length > 0" :data="studentList" border
+          ><el-table-column
+            type="index"
+            :index="indexMethod"
+            align="center"
+            label="序号"
+            width="50"
+          ></el-table-column>
+          <el-table-column prop="card_id" label="学号"></el-table-column>
+          <el-table-column prop="name" label="姓名"></el-table-column>
+          <el-table-column prop="school" label="学院"></el-table-column>
+          <el-table-column prop="grade" label="年级"></el-table-column>
+          <el-table-column prop="stu_class" label="班级"></el-table-column>
+          <el-table-column
+            prop="major"
+            label="专业"
+          ></el-table-column> </el-table
+      ></el-main>
+      <el-footer
+        ><el-pagination
+          v-if="studentList.length > 0"
+          background
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :total="totalStudents"
+          :page-sizes="[10, 20, 30, 50]"
+          @current-change="handlePageChange"
+          @size-change="handleSizeChange"
+          layout="total, sizes, prev, pager, next, jumper"
+        >
+        </el-pagination
+      ></el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -65,6 +87,9 @@ export default {
     this.fetchStudentList();
   },
   methods: {
+    indexMethod(index) {
+      return index + 1 + (this.currentPage - 1) * this.pageSize; // 返回表格序号
+    },
     async fetchStudentList() {
       const response = await getPage(this.currentPage, this.pageSize);
       this.studentList = response.data.data.rows; // 将数据赋值给studentList
@@ -86,10 +111,9 @@ export default {
       this.currentPage = currentPage;
       this.fetchStudentList();
     },
-  },
-  computed: {
-    pagedStudentList() {
-      return this.studentList; // 不需要分页处理，直接返回当前页的列表数据
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize;
+      this.fetchStudentList();
     },
   },
 };
